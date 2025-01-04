be788186762d5756195af9e2022f034ba4c7d221
Forrest Williams <ffwilliams2@alaska.edu>
2025-01-04 14:24:07 -0600

    add search test

--- tests/test_search.py
+++ tests/test_search.py
@@ -34,3 +34,21 @@ def test_add_surrounding_bursts(product):
         mock_search.assert_called_once_with(
             dataset='SLC-BURST', absoluteOrbit=1, polarization='VV', fullBurstID=burst_ids
         )
+
+
+def test_sanitize_group_search_inputs():
+    pols, swaths = search.sanitize_group_search_inputs()
+    assert pols == ['VV']
+    assert swaths == [None]
+
+    assert search.sanitize_group_search_inputs(polarizations=['HH'])[0] == ['HH']
+    assert search.sanitize_group_search_inputs(swaths=['IW2'])[1] == ['IW2']
+
+    with pytest.raises(ValueError, match='Invalid polarization*'):
+        search.sanitize_group_search_inputs(polarizations=['VV', 'BB'])
+
+    with pytest.raises(ValueError, match='Invalid swath*'):
+        search.sanitize_group_search_inputs(swaths=['IW1'], mode='EW')
+
+    with pytest.raises(ValueError, match='Invalid swath*'):
+        search.sanitize_group_search_inputs(swaths=['EW1'], mode='IW')
