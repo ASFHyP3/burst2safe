6f9669263c010f205bdc8b5653e3a13bc5dd5e76
mfangaritav <mf.angarita52@gmail.com>
2025-06-26 17:41:27 -0800

    mypy

--- src/burst2safe/utils.py
+++ src/burst2safe/utils.py
@@ -298,7 +298,7 @@ def get_bbox(extent):
     elif float(extent[1]) >= float(extent[3]):
         raise ValueError('The south latitude is larger than the north latitude')
     else:
-        return box(*[float(x) for x in extent])
+        return box(*[float(x) for x in extent], ccw=True)
 
 
 def reparse_args(args: Namespace, tool: str) -> Namespace:
